3e488457904b7129252cdc3ffb20ea93851b70bf
QiuShui <993865890@qq.com>
2022-03-06 15:18:31 +0800

    受控组件 受控组件和非受控组件

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./styles.css";
 
+// 受控组件案例
 export default function App() {
+  const [value, setValue] = useState({ amount: 0, type: "人民币" });
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setValue({ ...value, amount: event.target.value });
+  };
+  const selectChange = (event) => {
+    console.log(event.target.value);
+    setValue({ ...value, type: event.target.value });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input value={value.amount} onChange={handleChange} />
+      <select defaultValue={value.type} onChange={selectChange}>
+        <option value="人民币">人民币</option>
+        <option value="美元">美元</option>
+      </select>
     </div>
   );
 }
